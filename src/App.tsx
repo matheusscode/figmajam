b9870043e9abb53276fe5ae23a82a430b102f156
Matheus Santos <matheussdev3@gmail.com>
2023-08-10 00:46:32 -0300

    feat: adding new line style

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,21 @@ import ReactFlow, {
   addEdge,
   Node,
 } from "reactflow";
+import * as Toolbar from "@radix-ui/react-toolbar";
 import { zinc } from "tailwindcss/colors";
 import "reactflow/dist/style.css";
 
 import { Square } from "./components/nodes/Square";
+import DefaultEdge from "./components/edges/DefaultEdge";
 
 // Nodes, Edges
 
 const NODE_TYPES = {
   square: Square,
+};
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
 };
 
 const INITIAL_NODES = [
@@ -32,7 +38,7 @@ const INITIAL_NODES = [
   },
 
   {
-    id: "2",
+    id: crypto.randomUUID(),
     type: "square",
     position: {
       x: 1000,
@@ -50,20 +56,46 @@ export default function App() {
     return setEdges((edges) => addEdge(connection, edges));
   }, []);
 
+  const addSquareNode = () => {
+    setNodes(nodes => {
+      ...nodes,
+      {
+        id: crypto.randomUUID(),
+        type: 'square'
+        position: {
+          x: 200,
+          y: 400,
+        },
+        data: {}
+      }
+    });
+  };
+
   return (
     <div className="w-screen h-screen">
       <ReactFlow
         nodeTypes={NODE_TYPES}
-        nodes={INITIAL_NODES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
         edges={edges}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         onNodesChange={onNodesChange}
         connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: "default",
+        }}
       >
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
+
+      <Toolbar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
+        <Toolbar.Button
+          onClick={}
+          className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover: -translate-y-2"
+        />
+      </Toolbar.Root>
     </div>
   );
 }
--- src/components/edges/DefaultEdge.tsx
+++ src/components/edges/DefaultEdge.tsx
@@ -1,7 +1,32 @@
-import { EdgeProps } from "reactflow"
+import { EdgeProps, getSmoothStepPath } from "reactflow";
 
-export function DefaultEdge(props: EdgeProps) {
-    return (
-        
-    )
-} 
+export default function DefaultEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  markerEnd,
+}: EdgeProps) {
+  const [edgePath] = getSmoothStepPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  });
+
+  return (
+    <path
+      id={id}
+      style={style}
+      className="react-flow__edge-path stroke-2 stroke-zinc-300"
+      d={edgePath}
+      markerEnd={markerEnd}
+    />
+  );
+}
