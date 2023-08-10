df690a1c9edf17dcc505cfa11964da125c8d849d
matheusscode <matheussdev3@gmail.com>
2023-08-10 07:47:36 -0400

    feat: finallized first part square and edges

--- src/App.tsx
+++ src/App.tsx
@@ -56,20 +56,20 @@ export default function App() {
     return setEdges((edges) => addEdge(connection, edges));
   }, []);
 
-  const addSquareNode = () => {
-    setNodes(nodes => {
+  function addSquareNode() {
+    setNodes((nodes) => [
       ...nodes,
       {
         id: crypto.randomUUID(),
-        type: 'square'
+        type: "square",
         position: {
-          x: 200,
-          y: 400,
+          x: 750,
+          y: 350,
         },
-        data: {}
-      }
-    });
-  };
+        data: {},
+      },
+    ]);
+  }
 
   return (
     <div className="w-screen h-screen">
@@ -92,8 +92,8 @@ export default function App() {
 
       <Toolbar.Root className="fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden">
         <Toolbar.Button
-          onClick={}
-          className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover: -translate-y-2"
+          onClick={addSquareNode}
+          className="w-32 h-32 bg-violet-500 mt-6 rounded transition-transform hover:-translate-y-2"
         />
       </Toolbar.Root>
     </div>
